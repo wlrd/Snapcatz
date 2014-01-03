cd4129d73f73ba9dcc447892de98a733ec4ffde8
Tommy Lomont <tommy.lomont@gmail.com>
2014-01-03 03:03:25 -0500

    Adding random cats

---
+++ cat.py
@@ -0,0 +1,73 @@
+import re
+import urllib2
+from bs4 import BeautifulSoup
+
+from bottle import get, post, request, run
+from snapchat import Snapchat
+import getpass
+import urllib
+
+
+url = 'http://www.reddit.com/r/catpictures'
+conn = urllib2.urlopen(url)
+html = conn.read()
+soup = BeautifulSoup(html)
+a = []
+for elem in soup.findAll('a', href=re.compile('\.imgur\.com/')):
+	a.append(elem['href'])
+	#print a
+
+url = 'http://www.reddit.com/r/catpictures/?count=25&after=t3_1u5r74'
+conn = urllib2.urlopen(url)
+html = conn.read()
+soup = BeautifulSoup(html)
+#for elem in soup.findAll('a', href=re.compile('\.imgur\.com/')):
+	#print elem['href']
+
+url = 'http://www.reddit.com/r/catpictures/?count=75&after=t3_1u0dwn'
+conn = urllib2.urlopen(url)
+html = conn.read()
+soup = BeautifulSoup(html)
+#for elem in soup.findAll('a', href=re.compile('\.imgur\.com/')):
+	#print elem['href']
+
+
+from random import choice
+url_final = choice(a)
+while("domain" in url_final):
+	url_final = choice(a)
+print url_final
+
+@get('/login')
+def login():
+	return '''
+		<form name="input" action="/login" method="post">
+		Your Username: 
+		<input type="text" name="username">
+		Password:
+		<input type="password" name="password">
+		Recipient Username:
+		<input type="text" name="friend">
+		Url of Picture:
+		<input type="text" name="pic_url">
+		<input type="submit" value="Submit">
+		</form>
+		'''
+
+@post('/login')
+def do_login():
+	name = request.forms.get('username')
+	password = request.forms.get('password')
+	friend = request.forms.get('friend')
+	url = request.forms.get('pic_url')
+	urllib.urlretrieve(url_final, "1.jpg")
+	pic = "1.jpg"
+	s = Snapchat()
+	s.login(name, password)
+
+	# Send a snapchat
+	media_id = s.upload(Snapchat.MEDIA_IMAGE, pic)
+	s.send(media_id, friend)
+
+run(host='localhost', port=8080, debug=True)
+
